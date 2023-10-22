bf05f9304957aa998e62f503f03b997429562998
Aalam <aalamansari1712@gmail.com>
2023-10-22 22:45:22 +0530

    update

---
+++ app.py
@@ -0,0 +1,126 @@
+import joblib
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+filename = 'diabetes-prediction-rfc-model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+model1 = pickle.load(open('model1.pkl', 'rb'))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+bootstrap = Bootstrap(app)
+
+
+
+@app.route('/')
+def index():
+    return render_template("dashboard.html")
+
+
+@app.route("/dashboard")
+
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/diabetes")
+
+def diabetes():
+    return render_template("diabetes.html")
+
+
+@app.route("/kidney")
+
+def kidney():
+    return render_template("kidney.html")
+
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1, size)
+    if size == 7:
+        loaded_model = joblib.load('kidney_model.pkl')
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route("/predictkidney",  methods=['GET', 'POST'])
+def predictkidney():
+    if request.method == "POST":
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        if len(to_predict_list) == 7:
+            result = ValuePredictor(to_predict_list, 7)
+    if(int(result) == 1):
+        prediction = "Patient has a high risk of Kidney Disease, please consult your doctor immediately"
+    else:
+        prediction = "Patient has a low risk of Kidney Disease"
+    return render_template("kidney_result.html", prediction_text=prediction)
+
+
+##################################################################################
+
+df1 = pd.read_csv('diabetes.csv')
+
+# Renaming DiabetesPedigreeFunction as DPF
+df1 = df1.rename(columns={'DiabetesPedigreeFunction': 'DPF'})
+
+# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
+df_copy = df1.copy(deep=True)
+df_copy[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = df_copy[['Glucose', 'BloodPressure',
+                                                                                    'SkinThickness', 'Insulin',
+                                                                                    'BMI']].replace(0, np.NaN)
+
+# Replacing NaN value by mean, median depending upon distribution
+df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
+df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
+df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
+df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
+df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
+
+# Model Building
+
+X = df1.drop(columns='Outcome')
+y = df1['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Creating Random Forest Model
+
+classifier = RandomForestClassifier(n_estimators=20)
+classifier.fit(X_train, y_train)
+
+# Creating a pickle file for the classifier
+filename = 'diabetes-prediction-rfc-model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+
+#####################################################################
+
+
+@app.route('/predictt', methods=['POST'])
+def predictt():
+    if request.method == 'POST':
+        preg = request.form['pregnancies']
+        glucose = request.form['glucose']
+        bp = request.form['bloodpressure']
+        st = request.form['skinthickness']
+        insulin = request.form['insulin']
+        bmi = request.form['bmi']
+        dpf = request.form['dpf']
+        age = request.form['age']
+
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = classifier.predict(data)
+
+        return render_template('diab_result.html', prediction=my_prediction)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
